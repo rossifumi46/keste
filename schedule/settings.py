2e1ca784b5592eec22569fc89ace151ab9653813
Rustem <rossifumi.gp@gmail.com>
2017-10-11 23:21:29 +0600

    some

--- schedule/settings.py
+++ schedule/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '81+i6^cfl^bp056t1&i&z(%g*ci!9%ge5t0_tx%=ftiaq1j&1!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysterious-thicket-12979.herokuapp.com']
 
 
 # Application definition
